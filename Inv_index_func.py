2f77fc8a494a4865cdf857243095978737b31b5e
AhmedSultan2002 <ahmedsultanx2002@gmail.com>
2023-12-27 16:17:57 +0500

    multiword query implemented

--- Inv_index_func.py
+++ Inv_index_func.py
@@ -4,30 +4,30 @@ from glob import glob
 
 def inverted_func():
     #article_dic = {}
-    wordid_dict = {}
+    wordid_dict = {}        #impleneting word id dictionary, basically the content of the inverted index
 
 
-    Fwd_indexes = glob(r"D:\Vscode\NUST\Semester 3\DSA2\DSA-Project\Resources\fwd_indexes\*")
+    Fwd_indexes = glob(r"D:\Vscode\NUST\Semester 3\DSA2\DSA-Project\Resources\fwd_indexes\*")   #Accessing all forward indexes
     for index_file in Fwd_indexes:
         with open(index_file, 'r') as cur_file:
-            cur_index_json = json.load(cur_file)
-            for item in cur_index_json:
-                for word in item["words"]:
-                    word_id = int(word['id'])
-                    url = item["URL"]
+            cur_index_json = json.load(cur_file)                            #loading the contents of current fwd_index
+            for item in cur_index_json:                                     #accessing each Article
+                for word in item["words"]:                                  #accessing each word in word list associated with article
+                    word_id = int(word['id'])                               #taking word id, url and count out from each word
+                    url = item["URL"]           
                     count = word["count"]
                     
 
-                    if word_id not in wordid_dict:
-                        wordid_dict[word_id] = {}
+                    if word_id not in wordid_dict:                          #checking if this id has already been added to the dictionary
+                        wordid_dict[word_id] = {}                           #if not added initialize empty dictionary as value for wordid
                     
-                    wordid_dict[word_id][url] = count
+                    wordid_dict[word_id][url] = count                       #add the current url as the key for dictionary assocaited with word id with the value associated being count
 
     
 
     file_path = r"D:\Vscode\NUST\Semester 3\DSA2\DSA-Project\Resources\inv_index\inv_index.json"
     with open(file_path, 'w') as inv_index:
-        json.dump(wordid_dict, inv_index, indent=0)
+        json.dump(wordid_dict, inv_index, indent=0)                         #finally writing this to make inverted index
 
 
 
--- Search.py
+++ Search.py
@@ -1,7 +1,7 @@
 import json
 from nltk import word_tokenize
 import word_id_generator as WID
-
+import heapq
 
 
 
@@ -13,31 +13,45 @@ def Search_func(query):
 
     
     word_func = WID.word_id_generator(word_dict)
-
     separated_query = word_tokenize(query)
+    #print(separated_query)
+    URL_heap = []
     
     if len(separated_query) == 1:
         wor_id = word_func(separated_query[0].lower())
         filepath = f"D:\\Vscode\\NUST\\Semester 3\\DSA2\\DSA-Project\\Resources\\inv_index\\barrel_{wor_id//500}.json"
         with open(filepath, 'r') as current_barrel:
             loaded_barrel = json.load(current_barrel)
-            #print(loaded_barrel[str(wor_id)])
-            #print(loaded_barrel)
             id_dict = loaded_barrel[str(wor_id)]
             top_10_URLs = sorted(id_dict, key=id_dict.get, reverse=True)[:10]
-            for url in top_10_URLs:
-                print(url)
+            return top_10_URLs
+    elif len(separated_query) == 0:
+        return ["Please Enter correct search query"]
+    else:
+        URLs_List_in_order = []
+        for word in separated_query:
+            wor_id = word_func(word)
+            #print(wor_id)
+            filepath = f"D:\\Vscode\\NUST\\Semester 3\\DSA2\\DSA-Project\\Resources\\inv_index\\barrel_{wor_id//500}.json"
+            with open(filepath, 'r') as current_barrel:
+                loaded_barrel = json.load(current_barrel)
+                id_dict = loaded_barrel[str(wor_id)]
+                for key, value in id_dict.items():
+                    heapq.heappush(URL_heap, (-value, key))
+            while URL_heap:
+                value, key = heapq.heappop(URL_heap)
+                URLs_List_in_order.append(key)
+        return URLs_List_in_order
+        
 
 
-                
 
 
 
+        
+ToDisplay = Search_func("lucifer bomb")
+for stuff in ToDisplay:
+    print(stuff)
 
-
-
-
-
-Search_func("lucifer")
 
 
