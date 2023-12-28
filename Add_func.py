7ee05ada7d884aaad3a5ddfc2a1167e8df69bcb2
emanmuhammad05 <emanmuhammad120@gmail.com>
2023-12-28 12:12:12 +0500

    changes

--- Add_func.py
+++ Add_func.py
@@ -1,8 +1,32 @@
 import json
-import fwd_index_func as FWD
+import Add_func_dependencies as AFD
 from Inv_index_func import inverted_func as IF
 from Barrel_Func import Barrel_func as BF
-
+import word_id_generator as WID
 
 def add_file_func(JsonFileContent):
+    word_dict = {}
+    filepath = r"C:\Users\emanm\.vscode\DSA-Project\Search-Engine\Resources\nela-gt-2022\newsdata"
+    word_dict_path = r"C:\Users\emanm\.vscode\DSA-Project\Search-Engine\Resources\Word_Dictionary\lexicon.json"
+    
+    with open(word_dict_path, 'r') as wr_dic_file:
+        word_dict = json.load(wr_dic_file)
+    
+    word_func = WID.word_id_generator(word_dict)
+
+    AFD.FWD_index_parsing(JsonFileContent, filepath, word_func)
+    AFD.inverted_func(filepath)
+
+    newfilePath = r"C:\Users\emanm\.vscode\DSA-Project\Search-Engine\Resources\inv_index\new_inv_index.json"
+    with open(newfilePath) as inv_index:
+        inv_index_content = json.load(inv_index)
+        ids = list(inv_index_content.keys())
+        for key_id in ids:
+            barrelpath = "input barrel path here"
+            barrel_content = {}
+            with open(barrelpath, 'r') as reading_barrel:
+                barrel_content = json.load(reading_barrel)
+                barrel_content[int(key_id)].update(inv_index[key_id])
+
+
     
--- Add_func_dependencies.py
+++ Add_func_dependencies.py
@@ -96,3 +96,27 @@ def FWD_index_parsing(FileContent, index_path, word_func):
 
 
 
+def inverted_func(index_file):
+    #article_dic = {}
+    wordid_dict = {}        #impleneting word id dictionary, basically the content of the inverted index
+
+
+    with open(index_file, 'r') as cur_file:
+        cur_index_json = json.load(cur_file)                            #loading the contents of current fwd_index
+        for item in cur_index_json:                                     #accessing each Article
+            for word in item["words"]:                                  #accessing each word in word list associated with article
+                word_id = int(word['id'])                               #taking word id, url and count out from each word
+                url = item["URL"]           
+                count = word["count"]
+                
+
+                if word_id not in wordid_dict:                          #checking if this id has already been added to the dictionary
+                    wordid_dict[word_id] = {}                           #if not added initialize empty dictionary as value for wordid
+                
+                wordid_dict[word_id][url] = count                       #add the current url as the key for dictionary assocaited with word id with the value associated being count
+
+
+
+    file_path = r"D:\Vscode\NUST\Semester 3\DSA2\DSA-Project\Resources\inv_index\new_inv_index.json"
+    with open(file_path, 'w') as inv_index:
+        json.dump(wordid_dict, inv_index, indent=0) 
