82581acc39978ded0dbb0ef22a4135040326c760
AhmedSultan2002 <ahmedsultanx2002@gmail.com>
2023-12-29 06:27:42 +0500

    barrels changed to have better effieciency by having smaller barrels

--- Barrel_Func.py
+++ Barrel_Func.py
@@ -3,18 +3,18 @@ import json
 
 
 def Barrel_func(file_path):
-    barrel_size = 500
+    barrel_size = 10                           #amount of ids in each barrel
     with open(file_path, 'r') as full_index:
-        index_data = json.load(full_index)
+        index_data = json.load(full_index)   #load inverted index
 
 
         ids = list(index_data.keys())
         for i in range(0, len(ids), barrel_size):
-            barrel = {id : index_data[id] for id in ids[i:i + barrel_size]}
+            barrel = {id : index_data[id] for id in ids[i:i + barrel_size]}  #splice according to ids
 
 
-            with open(f"Resources/inv_index/barrel_{i//barrel_size}.json", 'w') as cur_barrel:
-                json.dump(barrel, cur_barrel, indent = 2)
+            with open(f"Resources/inv_index/barrel_{i//barrel_size}.json", 'w') as cur_barrel:  #create custom barrel number based on i which will start at 0
+                json.dump(barrel, cur_barrel, indent = 0)
 
 
 
