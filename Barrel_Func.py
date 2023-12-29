b1fc8d0e155519a6ab8d8db80c02e10ad266adcd
AhmedSultan2002 <ahmedsultanx2002@gmail.com>
2023-12-29 06:43:07 +0500

    i think it should work now

--- Barrel_Func.py
+++ Barrel_Func.py
@@ -3,11 +3,7 @@ import json
 
 
 def Barrel_func(file_path):
-<<<<<<< HEAD
     barrel_size = 10                           #amount of ids in each barrel
-=======
-    barrel_size = 500                           #amount of ids in each barrel
->>>>>>> fc136bb4614b20f33d7d4a5d777a73859adc3eeb
     with open(file_path, 'r') as full_index:
         index_data = json.load(full_index)   #load inverted index
 
@@ -18,11 +14,7 @@ def Barrel_func(file_path):
 
 
             with open(f"Resources/inv_index/barrel_{i//barrel_size}.json", 'w') as cur_barrel:  #create custom barrel number based on i which will start at 0
-<<<<<<< HEAD
                 json.dump(barrel, cur_barrel, indent = 0)
-=======
-                json.dump(barrel, cur_barrel, indent = 2)
->>>>>>> fc136bb4614b20f33d7d4a5d777a73859adc3eeb
 
 
 
